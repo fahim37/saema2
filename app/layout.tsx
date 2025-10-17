c2bfeb4b300ccb53cef5a1d5c9e6f8907cc709b3
Fahim <ahmed.fahim37@gmail.com>
2025-10-17 10:01:11 +0600

    banner added

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { GeistSans } from "geist/font/sans";
 import { GeistMono } from "geist/font/mono";
 import "./globals.css";
+import CookieBanner from "@/components/CookieBanner"; 
 
 export const metadata: Metadata = {
   title: "Saema",
@@ -10,6 +11,7 @@ export const metadata: Metadata = {
     icon: "/favicon.png",
   },
 };
+
 export default function RootLayout({
   children,
 }: Readonly<{
@@ -19,14 +21,17 @@ export default function RootLayout({
     <html lang="en">
       <head>
         <style>{`
-html {
-  font-family: ${GeistSans.style.fontFamily};
-  --font-sans: ${GeistSans.variable};
-  --font-mono: ${GeistMono.variable};
-}
+          html {
+            font-family: ${GeistSans.style.fontFamily};
+            --font-sans: ${GeistSans.variable};
+            --font-mono: ${GeistMono.variable};
+          }
         `}</style>
       </head>
-      <body>{children}</body>
+      <body>
+        {children}
+        <CookieBanner /> {/* Add the banner here */}
+      </body>
     </html>
   );
 }
